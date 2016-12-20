c8166b0b0d2dc927c49b9f9eaf2a37a3bb5718e8
Miriam Cortes <mcortes.umiami@gmail.com>
2016-12-19 17:09:35 -0800

    more tests

--- spec/game.spec.js
+++ spec/game.spec.js
@@ -76,47 +76,44 @@ describe('Game', function() {
       };
     });
 
-    // it('should can track multiple players plays', function() {
-    //   var ticTacToe = new Game();
-    //   ticTacToe.players("Mario","Luigi");
-    //   ticTacToe.play(3);
-    //   ticTacToe.play(5);
-    //
-    //   expect(ticTacToe.board[3]).toEqual("X");
-    //   expect(ticTacToe.board[5]).toEqual("O");
-    //   for (var i=1; i < 10; i++) {
-    //     if (i !== 3 && i !== 5) {
-    //       expect(ticTacToe.board[i]).toEqual(null);
-    //     }
-    //   };
-    // });
+    it('should can track multiple players plays', function() {
+      var daPlayers = ticTacToe.players("Mario","Luigi");
+      ticTacToe.play(3);
+      ticTacToe.play(5);
 
-    // it('should not let a player play a taken spot', function() {
-    //   var ticTacToe = new Game();
-    //   ticTacToe.players("Mario","Luigi");
-    //   ticTacToe.play(3);
-    //   expect(ticTacToe.board[3]).toEqual("X");
-    //   ticTacToe.play(3);
-    //   expect(ticTacToe.board[3]).toEqual("X");
-    //
-    //   expect(ticTacToe.allPlayers[0].turn).toEqual(false);
-    //   expect(ticTacToe.allPlayers[1].turn).toEqual(true);
-    // });
+      expect(ticTacToe.get("board")[3]).toEqual("X");
+      expect(ticTacToe.get("board")[5]).toEqual("O");
+      for (var i=1; i < 10; i++) {
+        if (i !== 3 && i !== 5) {
+          expect(ticTacToe.get("board")[i]).toEqual(null);
+        }
+      };
+    });
 
-    // it ('should set the winner', function() {
-    //   var ticTacToe = new Game();
-    //   ticTacToe.players("Mario","Luigi");
-    //   ticTacToe.play(1); // player 1
-    //   ticTacToe.play(4); // player 2
-    //   ticTacToe.play(2); // player 1
-    //   ticTacToe.play(5); // player 2
-    //
-    //   expect(ticTacToe.allPlayers[0].winner).toEqual(false);
-    //   ticTacToe.play(3); // player 1
-    //   expect(ticTacToe.allPlayers[0].winner).toEqual(true);
-    //   expect(ticTacToe.allPlayers[1].winner).toEqual(false);
-    //
-    // });
+    it('should not let a player play a taken spot', function() {
+      var daPlayers = ticTacToe.players("Mario","Luigi");
+      ticTacToe.play(3);
+      expect(ticTacToe.get("board")[3]).toEqual("X");
+      ticTacToe.play(3);
+      expect(ticTacToe.get("board")[3]).toEqual("X");
+
+      expect(daPlayers[0].get("turn")).toEqual(false);
+      expect(daPlayers[1].get("turn")).toEqual(true);
+    });
+
+    it ('should set the winner', function() {
+      var daPlayers = ticTacToe.players("Mario","Luigi");
+      ticTacToe.play(1); // player 1
+      ticTacToe.play(4); // player 2
+      ticTacToe.play(2); // player 1
+      ticTacToe.play(5); // player 2
+
+      expect(daPlayers[0].get("winner")).toEqual(false);
+      ticTacToe.play(3); // player 1
+      expect(daPlayers[0].get("winner")).toEqual(true);
+      expect(daPlayers[1].get("winner")).toEqual(false);
+
+    });
 
     // it('should not allow a play if there is a winner', function() {
     //   var ticTacToe = new Game();
